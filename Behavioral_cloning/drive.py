9d7d2af4191a1162f6423ffa8478413e5a2b7789
Kiarie <u4742829@anu.edu.au>
2017-02-08 17:28:00 +1100

    Working on YUV color channel

---
+++ Behavioral_cloning/drive.py
@@ -0,0 +1,113 @@
+import argparse
+import base64
+from datetime import datetime
+import os
+import shutil
+import cv2
+import math
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+from sklearn.preprocessing import normalize
+
+from keras.models import load_model
+
+sio = socketio.Server()
+app = Flask(__name__)
+model = None
+prev_image_array = None
+
+def prepImage(img):
+    #Get rid of top 1/5 of image, and bottom 25 pixels
+    shape = img.shape
+    img = img[math.floor(shape[0]/4):shape[0]-25, 0:shape[1]]
+    Y_channel = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    img = cv2.resize(normalize(Y_channel[:,:,0],  axis=1, norm='l1'),(64, 64), interpolation=cv2.INTER_AREA)  
+    return img
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
+        # The current speed of the car
+        speed = data["speed"]
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
+               
+        image_array = np.asarray(image)
+        image_array = prepImage(image_array)
+        image_array = image_array.reshape(64, 64, 1)
+
+        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        throttle = 0.2
+        print(steering_angle, throttle)
+        send_control(steering_angle, throttle)
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+    else:
+        # NOTE: DON'T EDIT THIS.
+        sio.emit('manual', data={}, skip_sid=True)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
+    args = parser.parse_args()
+
+    model = load_model(args.model)
+
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
